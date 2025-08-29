7561f312f514fed39a4d58c4f5941e64d951c597
OinkyOink2 <aydin.er@education.nsw.gov.au>
2025-08-29 10:24:30 +0000

    Fourth commit

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
 from flask import Flask, render_template, request
+from PIL import Image
+import io, base64
 
 app = Flask(__name__)
 
+CHARS = "@%#*+=-:. "
+
+def image_to_ascii(image, new_width=100):
+    width, height = image.size
+    ratio = height / width / 1.65
+    new_height = int(new_width * ratio)
+    image = image.resize((new_width, new_height))
+    image = image.convert("L")
+    pixels = image.getdata()
+    ascii_str = "".join([CHARS[pixel // 25] for pixel in pixels])
+    ascii_lines = [ascii_str[i:i+new_width] for i in range(0, len(ascii_str), new_width)]
+    return "\n".join(ascii_lines)
+
 @app.route("/", methods=["GET", "POST"])
 def home():
+    ascii_art = None
     if request.method == "POST":
         file = request.files["file"]
-    return render_template("index.html")
+        img = Image.open(file.stream)
+        ascii_art = image_to_ascii(img)
+    return render_template("index.html", ascii_art=ascii_art)
 
 if __name__ == "__main__":
     app.run(debug=True)
