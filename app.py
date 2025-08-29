240a0e5c40a3f0c9bcacdfcd62c96417810d8473
OinkyOink2 <aydin.er@education.nsw.gov.au>
2025-08-29 10:25:33 +0000

    Fifth commit

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
 from flask import Flask, render_template, request
 from PIL import Image
-import io, base64
 
 app = Flask(__name__)
 
-CHARS = "@%#*+=-:. "
+CHARSETS = {
+    "classic": "@%#*+=-:. ",
+    "dense": "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. ",
+}
 
-def image_to_ascii(image, new_width=100):
+def image_to_ascii(image, new_width=100, charset="classic"):
+    CHARS = CHARSETS[charset]
     width, height = image.size
     ratio = height / width / 1.65
     new_height = int(new_width * ratio)
     image = image.resize((new_width, new_height))
     image = image.convert("L")
     pixels = image.getdata()
-    ascii_str = "".join([CHARS[pixel // 25] for pixel in pixels])
+    ascii_str = "".join([CHARS[pixel * len(CHARS) // 256] for pixel in pixels])
     ascii_lines = [ascii_str[i:i+new_width] for i in range(0, len(ascii_str), new_width)]
     return "\n".join(ascii_lines)
 
@@ -22,8 +25,10 @@ def home():
     ascii_art = None
     if request.method == "POST":
         file = request.files["file"]
+        width = int(request.form.get("width", 100))
+        charset = request.form.get("charset", "classic")
         img = Image.open(file.stream)
-        ascii_art = image_to_ascii(img)
+        ascii_art = image_to_ascii(img, width, charset)
     return render_template("index.html", ascii_art=ascii_art)
 
 if __name__ == "__main__":
